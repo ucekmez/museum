aab62c0fb4c9f0ed0a5e067cb5432f6a7f5d0b85
Ugur Cekmez <ucekmez@gmail.com>
2018-07-14 20:43:00 +0300

    fix

--- api/API.py
+++ api/API.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import falcon
 from db import *
 from falcon_multipart.middleware import MultipartMiddleware
--- api/db.py
+++ api/db.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from mongoengine import *
 import datetime, json, requests, random, string
 import mongoengine_goodjson as gj
